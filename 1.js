f3561aa25ef99f7d6f28859e84c08498545ec090
xxx <1404294396@qq.com>
2020-06-16 10:18:53 +0800

    eeee

--- 1.js
+++ 1.js
@@ -3,4 +3,6 @@
 // shift + command +.
 ssssssss
 // vscode左边的第三个选项的changes里的文件就是你在工作区的文件，changes右边有一个加号，点击之后就相当于 git add .
-// 在上面有一个输入框，在这里输入commit的注释
+// 在上面有一个输入框，在这里输入commit的注释，然后ctrl+enter，就相当于 git commit -m'注释
+
+
